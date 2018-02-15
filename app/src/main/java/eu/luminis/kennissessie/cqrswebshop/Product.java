42b6c2e53bd89fbe4183b3c0b8bb8f5f23d2ad96
Mark Krijgsman <mark.krijgsman@luminis.eu>
2018-02-15 20:37:16 +0100

    Add test for Product

--- app/src/main/java/eu/luminis/kennissessie/cqrswebshop/Product.java
+++ app/src/main/java/eu/luminis/kennissessie/cqrswebshop/Product.java
@@ -1,5 +1,53 @@
 package eu.luminis.kennissessie.cqrswebshop;
 
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.GenericEventMessage;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import eu.luminis.kennissessie.cqrswebshop.api.command.CreateProductCommand;
+import eu.luminis.kennissessie.cqrswebshop.api.command.ReserveStockCommand;
+import eu.luminis.kennissessie.cqrswebshop.api.command.UndoStockReservationCommand;
+import eu.luminis.kennissessie.cqrswebshop.api.event.ProductReservedEvent;
+
+@Aggregate
+@Entity
 public class Product {
 
+    @Autowired
+    private transient EventBus eventBus;
+
+    @Id
+    @AggregateIdentifier
+    private UUID id;
+
+    private long amount;
+
+    // JPA happy :)
+    protected Product(){
+
+    }
+
+    @CommandHandler
+    public Product(CreateProductCommand createProductCommand){
+        // TODO: Implement
+    }
+
+    @CommandHandler
+    public void handle(ReserveStockCommand reserveStockCommand){
+        // TODO: Implement
+    }
+
+    @CommandHandler
+    public void handle(UndoStockReservationCommand undoStockReservationCommand){
+        // TODO: Implement
+
+    }
 }
